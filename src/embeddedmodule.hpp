45f0f1db659c2003a8001606d52adcc9e7512cf2
Manuel Schneider <manuelschneid3r@gmail.com>
2026-05-31 15:11:15 +0200

    fix: Loss of precision in `RankItem`

--- src/embeddedmodule.hpp
+++ src/embeddedmodule.hpp
@@ -436,7 +436,7 @@ PYBIND11_EMBEDDED_MODULE(albert, m)
 
     // Do not expose members to avoid unnecessary casts
     py::classh<RankItem>(m, "RankItem")
-        .def(py::init<shared_ptr<Item>,float>(),
+        .def(py::init<shared_ptr<Item>, double>(),
              py::arg("item"),
              py::arg("score"))
         ;
