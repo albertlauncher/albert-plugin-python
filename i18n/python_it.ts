99689a1b0ce3120aa09f83f91911344ee1b9b15b
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/python_it.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>ConfigWidget</name>
+    <message>
+        <source>Python version</source>
+        <translation>Versione di Python</translation>
+    </message>
+    <message>
+        <source>PyBind version</source>
+        <translation>Versione di PyBind</translation>
+    </message>
+    <message>
+        <source>API version</source>
+        <translation>Versione API</translation>
+    </message>
+    <message>
+        <source>Virtual environment</source>
+        <translation>Ambiente virtuale</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Apri</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Reimposta</translation>
+    </message>
+    <message>
+        <source>User plugin directory</source>
+        <translation>Directory plugin utente</translation>
+    </message>
+    <message>
+        <source>Terminal</source>
+        <translation>Terminale</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>No &apos;%1&apos; in $PATH.</source>
+        <translation>Nessun &apos;%1&apos; in $PATH.</translation>
+    </message>
+    <message>
+        <source>&apos;%1&apos; timed out (30s).</source>
+        <translation>&apos;%1&apos; timeout (30s).</translation>
+    </message>
+    <message>
+        <source>&apos;%1&apos; crashed.</source>
+        <translation>&apos;%1&apos; è crashed.</translation>
+    </message>
+    <message>
+        <source>&apos;%1&apos; finished with exit code: %2.</source>
+        <translation>&apos;%1&apos; terminato con codice di uscita: %2.</translation>
+    </message>
+    <message>
+        <source>Resetting the virtual environment requires a restart. Restart now?</source>
+        <translation>La reimpostazione dell&apos;ambiente virtuale richiede un riavvio. Riavviare ora?</translation>
+    </message>
+</context>
+</TS>
